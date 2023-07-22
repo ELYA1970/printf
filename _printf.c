9a44cc9e00ea7d5398956698963268e0460083e9
EL_YAZIDI <abdo.elyazidi2018@gmail.com>
2023-07-22 02:34:40 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -66,9 +66,21 @@ int _printf(const char *format, ...)
 			}
 			else
 			{
-				_putchar(*format);
+				_putchar('%');
 				count++;
+				if (*format)
+				{
+					_putchar(*format);
+					count++;
+				}
 			}
+			format++;
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+			format++;
 		}
 	}
 	return (count);
