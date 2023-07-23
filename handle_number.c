ba95cb75b0c634711a24f44b57d86be05eb7fff8
EL_YAZIDI <abdo.elyazidi2018@gmail.com>
2023-07-23 07:23:16 -0700

    task 1

---
+++ handle_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * length - give the length of a number
+ * @number: an integer
+ * Return: return the length of number
+ */
+int length(int number)
+{
+	if (number == 0)
+		return (0);
+	return (1 + length(number / 10));
+}
+
+/**
+ * handle_number - print numbers
+ * @number: an integer
+ * Return: an integer printed
+ */
+int handle_number(int number)
+{
+	int likay7seb = 0;
+
+    if (number < 0) {
+        likay7seb++;
+        putchar('-'); 
+        number = -number; 
+    }
+
+    likay7seb += length(number);
+
+    if (number >= 10) {
+        handle_number(number / 10);
+        handle_number(number % 10);
+    } else {
+        putchar(number + '0');
+    }
+    return likay7seb;
+}
